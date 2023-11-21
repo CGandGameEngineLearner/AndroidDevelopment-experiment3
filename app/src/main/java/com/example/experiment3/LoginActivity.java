18b3caffbce4b7b8f0eff2fe0ae031fb55ffe786
CGandGameEngineLearner <1353321741@qq.com>
2023-11-21 16:56:05 +0800

    feat: LoginActivity completed

--- app/src/main/java/com/example/experiment3/LoginActivity.java
+++ app/src/main/java/com/example/experiment3/LoginActivity.java
@@ -4,11 +4,97 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.RadioGroup;
+
+
 public class LoginActivity extends AppCompatActivity {
+
+    protected EditText mNameOrMail,mPassword;
+
+    protected Button mLogin;
+
+    protected TextView mLoginState;
+
+    protected RadioGroup mRadioGroup;
+
+
+    private void init()
+    {
+        mNameOrMail=findViewById(R.id.editViewNameOrMail);
+        mPassword=findViewById(R.id.editViewPassword);
+        mLogin=findViewById(R.id.buttonLogin);
+        mRadioGroup=findViewById(R.id.radioGroup);
+        mLoginState=findViewById(R.id.textViewLoginState);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+    }
+
+    void setLoginState(int state)
+    {
+        if(state==1)
+        {
+            mLoginState.setHint("登陆成功");
+        }
+        else if(state==0)
+        {
+            mLoginState.setHint("登陆失败");
+        }
+        else {
+            mLoginState.setHint("");
+        }
+    }
+
+    private void login()
+    {
+        int selectedRadioButtonId = mRadioGroup.getCheckedRadioButtonId();
+        String NameOrMail=mNameOrMail.getText().toString();
+        String Password=mPassword.getText().toString();
+        if (selectedRadioButtonId==R.id.nameButton)
+        {
+            if(
+                    NameOrMail.equals("zhangsan")
+                    &&Password.equals("123456")
+            )
+            {
+                setLoginState(1);
+            }
+            else {
+                setLoginState(0);
+            }
+        }
+        else if(selectedRadioButtonId==R.id.emalButton)
+        {
+            if(
+                    NameOrMail.equals("zhangsan@163.com")
+                            &&Password.equals("12345678")
+            )
+            {
+                setLoginState(1);
+            }
+            else {
+                setLoginState(0);
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "您没有选择登录方式", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/experiment3/MainActivity.java
+++ app/src/main/java/com/example/experiment3/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.experiment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    protected Button buttonUserLogin,buttonPersonalCenter,buttonLogin,buttonDetails,buttonChangePassword,buttonRegister,buttonQQ;
+
+
+    protected void initButtons()
+    {
+        buttonUserLogin =findViewById(R.id.buttonUserLogin);
+        buttonPersonalCenter=findViewById(R.id.buttonPersonalCenter);
+        buttonLogin=findViewById(R.id.buttonLogin);
+        buttonQQ=findViewById(R.id.buttonQQ);
+        buttonDetails=findViewById(R.id.buttonDetails);
+        buttonRegister=findViewById(R.id.buttonRegister);
+        buttonChangePassword=findViewById(R.id.buttonChangePassword);
+
+        // 设置按钮点击事件
+        buttonUserLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建并启动跳转到 EmailActivity 的 Intent
+                Intent emailIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(emailIntent);
+                Log.d("MainActivity","LoginActivity");
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
     }
 }
